e0197d7195fc179b3120bd0e5ed082b3b68c127c
WebDev955 <austingeisinger955@gmail.com>
2025-09-27 22:29:45 -0400

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDHOq__gt6HEkEkCDfxquDCpSheirR5qEA",
+  authDomain: "bingelog-e89f5.firebaseapp.com",
+  projectId: "bingelog-e89f5",
+  storageBucket: "bingelog-e89f5.firebasestorage.app",
+  messagingSenderId: "1027526846013",
+  appId: "1:1027526846013:web:ac40efa1ca0b6c4e50ec54",
+  measurementId: "G-N05RJMX3YG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Export helpers so other files can use them
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
