964c790a05ff027f303915c624cba03bb4144a92
WebDev955 <austingeisinger955@gmail.com>
2025-10-01 20:06:52 -0400

    changes to Auth

---
+++ src/components/Auth.jsx
@@ -0,0 +1,13 @@
+
+function Auth() {
+    return (
+            <div>
+                <input placeholder="Email..."/>
+                <input placeholder="Password..."/>
+            </div>
+    );
+
+}
+
+
+export default Auth
